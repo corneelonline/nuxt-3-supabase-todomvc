85fef29b96b7566ceacdbf3d5af3ee7f6a3b4aa7
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-01 15:24:41 +0200

    Add supabase config

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup>
+import { createClient } from "@supabase/supabase-js/dist/main/index.js";
+
+const { supabaseUrl, supabasePublicKey } = useRuntimeConfig();
+const supabase = createClient(supabaseUrl, supabasePublicKey);
+
+console.log(supabase);
+</script>
+
 <template>
   <div>
     <h1>Nuxt 3 Supabase TodoMVC</h1>
+    <pre>{{ supabase }}</pre>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  publicRuntimeConfig: {
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabasePublicKey: process.env.SUPABASE_PUBLIC_KEY
+  },
 })
