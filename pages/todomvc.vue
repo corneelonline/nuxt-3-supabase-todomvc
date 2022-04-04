d6acfe6c63c9ecf8ad0c866dc172722565c70b98
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2022-04-04 14:25:37 +0200

    Add ability to add item to the database

--- pages/todomvc.vue
+++ pages/todomvc.vue
@@ -1,7 +1,39 @@
+<script setup>
+import { ref } from "vue";
+import { createClient } from "@supabase/supabase-js/dist/main/index.js";
+import { store } from "../store/index.js";
+
+const { supabaseUrl, supabasePublicKey } = useRuntimeConfig();
+const supabase = createClient(supabaseUrl, supabasePublicKey);
+
+const newTask = ref("");
+
+const taskList = ref([]);
+
+const addTask = async () => {
+  try {
+    const { data, error } = await supabase.from("todos").insert([
+      {
+        user_id: store.user.id,
+        task: newTask.value,
+        is_complete: false,
+      },
+    ]);
+    if (error) throw error;
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
 <template>
   <h1>Todo MVC</h1>
+  <input type="text" v-model="newTask" @keyup.enter="addTask" />
+  <h2>List of tasks</h2>
+  <ul>
+    <li v-for="(task, index) in taskList" :key="task.task + index">
+      <input type="checkbox" v-model="task.isComplete" /> {{ task.task }}
+    </li>
+  </ul>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
